72c4cd900e2be186e40370f2bb2c28ced8080875
Juan pablo Jofre <jpjofre@webdox.cl>
2018-10-28 19:14:36 -0300

    adds endpoint to get a specific hashtag & returns uri in every hashtag response

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_pymongo import PyMongo
 from flask_restful import (Resource, reqparse, fields, marshal)
+from bson import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("DB")
@@ -13,7 +14,8 @@ hashtag_parser = reqparse.RequestParser()
 hashtag_parser.add_argument("hashtag", required=True)
 
 hashtag_fields = {"_id": fields.String, "name": fields.String,
-                  "user_id": fields.Integer, "date": fields.DateTime}
+                  "user_id": fields.Integer, "date": fields.DateTime,
+                  "uri": fields.Url('hashtag')}
 
 
 class HashtagListAPI(Resource):
@@ -26,8 +28,6 @@ class HashtagListAPI(Resource):
             help="No hashtag name provided",
             location="json",
         )
-        self.reqparse.add_argument("description", type=str, default="",
-                                   location="json")
         self.user_id = 1
         super(HashtagListAPI, self).__init__()
 
@@ -44,28 +44,27 @@ class HashtagListAPI(Resource):
         MONGO.db.hashtags.insert_one(hashtag)
         _last_added = MONGO.db.hashtags.find().sort([("$natural", -1)]).limit(1)
         last_added = [hashtag for hashtag in _last_added]
-        return {'hashtags': [marshal(hashtag, hashtag_fields)
+        return {'hashtag': [marshal(hashtag, hashtag_fields)
                              for hashtag in last_added]}, 201
 
 
-# class HashtagAPI(Resource):
-#     def __init__(self):
-#         self.reqparse = reqparse.RequestParser()
-#         self.reqparse.add_argument("name", type=str, location="json")
-#         super(HashtagAPI, self).__init__()
+class HashtagAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        super(HashtagAPI, self).__init__()
 
-#     def get(self, id):
-#         hashtag = MONGO.db.hashtags.find_one_or_404({"_id": id})
-#         if len(hashtag) == 0:
-#             abort(404)
-#         return {"hashtag": marshal(hashtag, hashtag_fields)}
+    def get(self, _id):
+        object_id = ObjectId(_id)
+        hashtag = MONGO.db.hashtags.find_one_or_404({"_id": object_id})
+        return {"hashtag": marshal(hashtag, hashtag_fields)}
 
 
 api = Api(app)
 
 # add api resources
 api.add_resource(HashtagListAPI, "/api/v1.0/hashtags", endpoint="hashtags")
-# api.add_resource(HashtagAPI, "/api/v1.0/hashtags/<int:id>", endpoint="hashtag")
+api.add_resource(HashtagAPI, "/api/v1.0/hashtags/<string:_id>", endpoint="hashtag")
 
 if __name__ == "__main__":
     app.run(debug=True)
